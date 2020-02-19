797eecd96745a34d90a8d718bd5ea3f5799c34de
AntoineDuf <antoine.dufayet@gmail.com>
2020-02-19 15:14:56 +0100

    Update the UIAlert method.

--- LeBaluchon/LeBaluchon/Extensions/UIViewController+UIAlertController.swift
+++ LeBaluchon/LeBaluchon/Extensions/UIViewController+UIAlertController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 extension UIViewController {
-    func alert(title: String, message: String) {
+    func alert(title: String? = nil, message: String? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
         alert.addAction(ok)
         present(alert, animated: true, completion: nil)
     }
+    
+    enum ErrorText: Error {
+        case connexionIssue
+        case serverIssue
+    }
+    
+//    func ignitErrorMessage() throws {
+//        if {
+//            throw ErrorText.connexionIssue
+//        } else if {
+//            throw ErrorText.serverIssue
+//        }
+//    }
+//
+//    func displayError() {
+//        do {
+//            try ignitErrorMessage()
+//        } catch ErrorText.connexionIssue {
+//            print("Vérifiez votre connexion.")
+//        } catch ErrorText.serverIssue {
+//            print("Problème de communication avec le serveur")
+//        }
+//    }
 }
